9803ca01cd592c23b89386401e738708bcf244d0
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-09-18 12:49:52 +0200

    update

--- main.py
+++ main.py
@@ -58,4 +58,126 @@ if int(choice_2_space) == 5 and int(choice_1) == 1:
         rand_int_2 = random.randint(0,1000000)
         user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
         solution = int(rand_int_1)+int(rand_int_2)
-        print(solution == user_solution, ', the correct solution is: ', solution)          
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 1 and int(choice_1) == 2:
+    while True:
+        rand_int_1 = random.randint(0,100)
+        rand_int_2 = random.randint(0,100)
+        user_solution = int(input('%i - %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)-int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 2 and int(choice_1) == 2:
+    while True:
+        rand_int_1 = random.randint(0,1000)
+        rand_int_2 = random.randint(0,1000)
+        user_solution = int(input('%i - %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)-int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+        
+if int(choice_2_space) == 3 and int(choice_1) == 2:
+    while True:
+        rand_int_1 = random.randint(0,10000)
+        rand_int_2 = random.randint(0,10000)
+        user_solution = int(input('%i - %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)-int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 4 and int(choice_1) == 2:
+    while True:
+        rand_int_1 = random.randint(0,100000)
+        rand_int_2 = random.randint(0,100000)
+        user_solution = int(input('%i - %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)-int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 5 and int(choice_1) == 2:
+    while True:
+        rand_int_1 = random.randint(0,1000000)
+        rand_int_2 = random.randint(0,1000000)
+        user_solution = int(input('%i - %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)-int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 1 and int(choice_1) == 3:
+    while True:
+        rand_int_1 = random.randint(0,100)
+        rand_int_2 = random.randint(0,100)
+        user_solution = int(input('%i * %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)*int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 2 and int(choice_1) == 3:
+    while True:
+        rand_int_1 = random.randint(0,1000)
+        rand_int_2 = random.randint(0,1000)
+        user_solution = int(input('%i * %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)*int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+        
+if int(choice_2_space) == 3 and int(choice_1) == 3:
+    while True:
+        rand_int_1 = random.randint(0,10000)
+        rand_int_2 = random.randint(0,10000)
+        user_solution = int(input('%i * %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)*int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 4 and int(choice_1) == 3:
+    while True:
+        rand_int_1 = random.randint(0,100000)
+        rand_int_2 = random.randint(0,100000)
+        user_solution = int(input('%i * %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)*int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 5 and int(choice_1) == 3:
+    while True:
+        rand_int_1 = random.randint(0,1000000)
+        rand_int_2 = random.randint(0,1000000)
+        user_solution = int(input('%i * %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)*int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+
+if int(choice_2_space) == 1 and int(choice_1) == 4:
+    while True:
+        rand_int_1 = random.randint(0,100)
+        rand_int_2 = random.randint(0,100)
+        user_solution = int(input('%i / %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)/int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: %.4f'%solution)
+
+if int(choice_2_space) == 2 and int(choice_1) == 4:
+    while True:
+        rand_int_1 = random.randint(0,1000)
+        rand_int_2 = random.randint(0,1000)
+        user_solution = int(input('%i / %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)/int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: %.4f'%solution)
+        
+if int(choice_2_space) == 3 and int(choice_1) == 4:
+    while True:
+        rand_int_1 = random.randint(0,10000)
+        rand_int_2 = random.randint(0,10000)
+        user_solution = int(input('%i / %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)/int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: %.4f'%solution)
+
+if int(choice_2_space) == 4 and int(choice_1) == 4:
+    while True:
+        rand_int_1 = random.randint(0,100000)
+        rand_int_2 = random.randint(0,100000)
+        user_solution = int(input('%i / %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)/int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: %.4f'%solution)
+
+if int(choice_2_space) == 5 and int(choice_1) == 4:
+    while True:
+        rand_int_1 = random.randint(0,1000000)
+        rand_int_2 = random.randint(0,1000000)
+        user_solution = int(input('%i / %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)/int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: %.4f'%solution)
+
