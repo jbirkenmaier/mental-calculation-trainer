1f9e1e0e37318b0bdc6e98bf30a8e93e7cc85dda
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-09-18 12:36:39 +0200

    update

--- main.py
+++ main.py
@@ -13,19 +13,49 @@ if int(choice_1)<=4:
     choice_2_space = input('{:<30} {:<10}\n'
                            '{:<30} {:<10}\n'
                            '{:<30} {:<10}\n'
+                           '{:<30} {:<10}\n'
                            '{:<30} {:<10}\n'.format('For numbers from 0 to 100','enter 1',
                                                     'For numbers from 0 to 1000','enter 2',
                                                     'For numbers from 0 to 10000','enter 3',
                                                     'For numbers from 0 to 100000', 'enter 4',
                                                     'For numbers from 0 to 1000000', 'enter 5'))
     
-if int(choice_2_space) == 1:
+if int(choice_2_space) == 1 and int(choice_1) == 1:
     while True:
         rand_int_1 = random.randint(0,100)
         rand_int_2 = random.randint(0,100)
         user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
         solution = int(rand_int_1)+int(rand_int_2)
         print(solution == user_solution, ', the correct solution is: ', solution)
-        
 
-                  
+if int(choice_2_space) == 2 and int(choice_1) == 1:
+    while True:
+        rand_int_1 = random.randint(0,1000)
+        rand_int_2 = random.randint(0,1000)
+        user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)+int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+        
+if int(choice_2_space) == 3 and int(choice_1) == 1:
+    while True:
+        rand_int_1 = random.randint(0,10000)
+        rand_int_2 = random.randint(0,10000)
+        user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)+int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 4 and int(choice_1) == 1:
+    while True:
+        rand_int_1 = random.randint(0,100000)
+        rand_int_2 = random.randint(0,100000)
+        user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)+int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)
+
+if int(choice_2_space) == 5 and int(choice_1) == 1:
+    while True:
+        rand_int_1 = random.randint(0,1000000)
+        rand_int_2 = random.randint(0,1000000)
+        user_solution = int(input('%i + %i = ' %(rand_int_1,rand_int_2)))
+        solution = int(rand_int_1)+int(rand_int_2)
+        print(solution == user_solution, ', the correct solution is: ', solution)          
