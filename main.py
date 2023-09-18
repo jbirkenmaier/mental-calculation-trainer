ea525f50b1749d3309a78441aa6b9996622c633d
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-09-18 12:09:42 +0200

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+import function_library as fl
+import random 
+
+choice_1 = input('{:<30} {:<10}\n'
+                 '{:<30} {:<10}\n'
+                 '{:<30} {:<10}\n'
+                 '{:<30} {:<10}\n'.format('For integer addition (+)','enter 1',
+                                          'For integer subtraction (-)','enter 2',
+                                          'For integer multiplication (*)','enter 3',
+                                          'For integer division (/)', 'enter 4'))
+      
+
+                  
